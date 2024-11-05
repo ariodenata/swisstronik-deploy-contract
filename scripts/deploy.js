5f9330aa839207be193a5ad09f7e16cfa3138432
ariodenata <gezaelshirazy@gmail.com>
2024-11-05 16:18:03 +0000

    Initial commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,20 +1,16 @@
 const hre = require("hardhat");
 
 async function main() {
-  /**
-   * @dev make sure the first argument has the same name as your contract in the Hello_swtr.sol file
-   * @dev the second argument must be the message we want to set in the contract during the deployment process
-   */
-  const contract = await hre.ethers.deployContract("Swisstronik", ["Hello Swisstronik!!"]);
+  const [deployer] = await hre.ethers.getSigners();
+
+  const ContractFactory = await hre.ethers.getContractFactory("ZunXBT");
+  const contract = await ContractFactory.deploy(deployer.address);
 
   await contract.waitForDeployment();
 
-  console.log(`Swisstronik contract deployed to ${contract.target}`);
+  console.log(`Your NFT Contract Address: ${contract.target}`);
 }
 
-//DEFAULT BY HARDHAT:
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
